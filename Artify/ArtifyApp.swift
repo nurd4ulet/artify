806cbdd85a0ecfc3c35014e5bc139758cb7bb403
nurd4ulet <102824957+nurd4ulet@users.noreply.github.com>
2022-12-24 20:15:44 +0600

    Add files via upload

---
+++ Artify/ArtifyApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct ArtifyApp: App {
+
+            var body: some Scene {
+                WindowGroup {
+                    ContentView()
+            }
+        }
+    }
